ff8431ded10db5ec8360aecfc47d47956fbf046f
Vighnesh Kadam <vighneshkadamdc@gmail.com>
2023-03-13 19:43:12 +0530

    port number changed

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const dbo = require("./db/conn");
 
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
