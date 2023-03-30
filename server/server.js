481d152a2b1828c2a2c451d7a2e5dd4261090fe1
Esakkiappan Thevar <thevarbalan32@gmail.com>
2023-03-30 17:28:43 +0530

    updated cors rules

--- server/server.js
+++ server/server.js
@@ -12,7 +12,25 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://railwaygrievances.netlify.app'
+}));
+
+
+app.use((req, res, next) => {
+
+
+  res.header('Access-Control-Allow-Origin', 'https://railwaygrievances.netlify.app');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  // console.log("Current user ",res.locals.user)
+  next();
+});
+
+
 app.use(express.json());
 app.use(jsonParser,require("./routes/record"));
 
