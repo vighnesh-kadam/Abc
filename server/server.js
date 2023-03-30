ed572c3e70d9288c63cd9be3f3f42fc8e8a51c6e
Esakkiappan Thevar <thevarbalan32@gmail.com>
2023-03-30 18:53:53 +0530

    added debug code

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
 
+  consolee.log("check",req.url,req)
 
   res.header('Access-Control-Allow-Origin', 'https://railwaygrievances.netlify.app');
   res.header('Access-Control-Allow-Credentials', true);
