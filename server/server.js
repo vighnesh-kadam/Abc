6c5579733333917431646440a3b887037a7739c6
Esakkiappan Thevar <thevarbalan32@gmail.com>
2023-04-01 13:07:20 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -20,7 +20,11 @@ app.use((req, res, next) => {
 
   
 
-  res.header('Access-Control-Allow-Origin', 'https://railwaygrievances.netlify.app');
+  // res.header('Access-Control-Allow-Origin', 'https://railwaygrievances.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://railwaygrievances.netlify.app');
+  res.append('Access-Control-Allow-Origin', 'http://localhost:3001');
+
+
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
