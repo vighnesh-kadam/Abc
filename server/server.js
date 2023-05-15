b5702b67e6749c1048f4326a588443fd099072bd
Esakkiappan Thevar <thevarbalan32@gmail.com>
2023-05-15 20:46:40 +0530

    updated cors rule

--- server/server.js
+++ server/server.js
@@ -21,6 +21,10 @@ app.use((req, res, next) => {
   
 
   res.header('Access-Control-Allow-Origin', 'https://railwaygrievances.netlify.app');
+
+  //  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+
+
   // res.setHeader('Access-Control-Allow-Origin', 'https://railwaygrievances.netlify.app');
   // res.append('Access-Control-Allow-Origin', 'http://localhost:3001');
 
